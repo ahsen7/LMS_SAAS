0a2c755a29e34f76b73d664c744858ae765f8713
Muhammad Ahsen <ahsen.mehmood412@gmail.com>
2025-06-12 19:29:20 +0500

    Navbar Completed

---
+++ components/navbar/NavItems.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const nitems = [
+    { label: 'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'Subscriptions', href: '/subscriptions' },
+    { label: 'Profile', href: '/myprofile' },
+]
+
+const NavItems = () => {
+    const pathName = usePathname();
+    return (
+        <nav className='flex items-center gap-4'>
+            {nitems.map(({ label, href }) => (
+                <Link
+                    href={href}
+                    key={label}
+                    className={cn(pathName === href && 'text-primary font-semibold')}
+                >
+                    {label}
+                </Link>
+            ))}
+        </nav>
+    )
+}
+
+export default NavItems
